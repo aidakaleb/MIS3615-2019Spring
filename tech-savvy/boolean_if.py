0eee188549fb3fa9ca306b8d0eb8096fb00a689a
Zhi Li <zli@babson.edu>
2019-03-16 15:09:55 -0400

    (almost) finish saturday

---
+++ tech-savvy/boolean_if.py
@@ -0,0 +1,6 @@
+is_in_US = True
+
+if is_in_US:
+    print('in US!')
+else:
+    print('not')
--- tech-savvy/chapter2.py
+++ tech-savvy/chapter2.py
@@ -1,9 +1,64 @@
-print(3.14e-2)
+## Numbers
+
+# print(3.14e-2)
 
 # length_of_number = len(str(2**1000000))
 #
 # print(length_of_number)
 
 
-print(len('Maddison'))
+# print(len('Maddison'))
+#
+# import math
+#
+# print(math.pi)
+# print(math.sqrt(25))
 
+
+import random
+
+# print(random.random())
+# print(int(random.random()*100)) # a random integer between 0 and 100
+#
+# print(random.randint(1, 6))  # a random integer between 1 and 6
+#
+# print(random.choice(["Valerie", "Aob",
+#                      "Maddison", "Grace"])) # a random name in the list
+#
+#
+# print(random.choice([2, 5, 7587]))
+
+# lower = int(input("Please a number  as the lower bound:"))
+# upper = int(input("another one as upper bound:"))
+# print(random.randint(lower, upper))
+
+
+## Strings
+# I'm saying "OK".
+
+
+# print("I'm saying \"OK\".")
+#
+# print('I\'m learning\n\nPython.')
+
+
+## Boolean
+
+print(3 > 2)
+print(3 > 5)
+
+print(3 > 2 and 3 > 5)
+print(3 > 2 or 3 > 5 or False or 100 < 100)
+
+age = int(input("How old are you?"))
+
+is_in_US = True
+
+answer = input('Are you in US?')
+if answer == 'no':
+    is_in_US = False
+
+if age < 21 and is_in_US:  # boolean
+    print('Sorry, you cannot.')
+else:
+    print('Yes, you can.')
